ac7e5b813ea98b634afcd538bb3102840670a6d6
Vlad Zaharia <hey@vlad.gg>
2023-03-14 18:32:46 -0700

    Remove config permission set

--- configure/home/fix-ssh-perms.sh
+++ configure/home/fix-ssh-perms.sh
@@ -2,10 +2,8 @@
 
 set -e # -e: exit on error
 
-ls -la $HOME/.ssh
-
 chown -R 1000:0 $HOME/.ssh
 chmod 700 $HOME/.ssh
-chmod 600 $HOME/.ssh/config
+#chmod 600 $HOME/.ssh/config
 chmod 600 $HOME/.ssh/id_rsa
 chmod 644 $HOME/.ssh/id_rsa.pub
