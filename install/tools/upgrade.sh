c6954ae072dc38b9332e5fceb8b2db3ff763ca49
Vlad Zaharia <hey@vlad.gg>
2025-02-26 20:30:48 +0000

    Update upgrade.sh

--- install/tools/upgrade.sh
+++ install/tools/upgrade.sh
@@ -2,4 +2,4 @@
 set -ex
 # Upgrade all packages
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-confnew -o Dpkg::Options::="--force-confnew"
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
